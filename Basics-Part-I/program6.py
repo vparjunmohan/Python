9b514874af222769f66d12365bea14506dea8964
Arjun <62532677+vparjunmohan@users.noreply.github.com>
2020-03-23 19:09:15 +0530

    Delete program6.py

--- Basics-Part-I/program6.py
+++
@@ -1,9 +0,0 @@
-"Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers."
-
-values = input('Enter comma-separated numbers: ')
-
-list = values.split(',')
-tuple = tuple(list)
-
-print('List: ',list)
-print('Tuple: ',tuple)
