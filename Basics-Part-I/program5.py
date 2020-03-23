af8c604053996f5a89c52380d495a83c674b5caf
Arjun <62532677+vparjunmohan@users.noreply.github.com>
2020-03-23 19:11:56 +0530

    Delete program5.py

--- Basics-Part-I/program5.py
+++
@@ -1,6 +0,0 @@
-"Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them."
-
-firstName = input('Enter first name: ')
-lastName = input('Enter last name: ')
-
-print(lastName + " " + firstName)
