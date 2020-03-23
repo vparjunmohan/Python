1c3957cb4fbc79fba93ca9fccbc4b9fc9173ffc3
vparjunmohan <vparjunmohan@gmail.com>
2020-03-23 19:06:57 +0530

    program6 added

---
+++ Basics-Part-I/program2.py
@@ -0,0 +1,6 @@
+"Write a Python program to get the Python version you are using."
+
+import sys
+
+print('\nPython Version \n',sys.version)
+print('\nPython Version Info. \n', sys.version_info)
---
+++ Basics-Part-I/program3.py
@@ -0,0 +1,6 @@
+"Write a Python program to display the current date and time."
+
+import datetime
+
+now = datetime.datetime.now()
+print('Current date and time is',now)
