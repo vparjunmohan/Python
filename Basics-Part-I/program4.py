2207bbd8fec7205feafe4ca6fbe9ec42f1023c19
Arjun <62532677+vparjunmohan@users.noreply.github.com>
2020-03-23 19:12:06 +0530

    Delete program4.py

--- Basics-Part-I/program4.py
+++
@@ -1,7 +0,0 @@
-"Write a Python program which accepts the radius of a circle from the user and compute the area."
-
-import math
-
-r = float(input('Enter radius of the circle: '))
-area = math.pi*r**2
-print('Area of the with radius',r,'is',area)
