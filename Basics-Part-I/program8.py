31a309ad04f628a89bd14a4f13efe090d6506c58
vparjunmohan <vparjunmohan@gmail.com>
2020-03-24 09:31:59 +0530

    p8 added

---
+++ Basics-Part-I/program8.py
@@ -0,0 +1,5 @@
+'''Write a Python program to display the first and last colors from the following list.
+color_list = ["Red","Green","White" ,"Black"]'''
+
+color_list = ["Red","Green","White" ,"Black"]
+print('First color is',color_list[0],'and Last color is',color_list[-1])
