0321edc847b3c578b6ea98a91249d1cd48bceb96
vparjunmohan <vparjunmohan@gmail.com>
2020-03-26 18:29:36 +0530

    p10 added

---
+++ Basics-Part-I/program10.py
@@ -0,0 +1,8 @@
+'''Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
+Sample value of n is 5
+Expected Result : 615'''
+
+n = int(input('Sample value of n is '))
+
+print(int('{}'.format(n))+int('{}{}'.format(n,n))+int('{}{}{}'.format(n,n,n)))
+
