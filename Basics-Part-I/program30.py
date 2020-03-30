aef78689ac0cc4dfc85e2d891a46459b0a42d3c2
vparjunmohan <vparjunmohan@gmail.com>
2020-03-30 12:26:27 +0530

    p30 added

---
+++ Basics-Part-I/program30.py
@@ -0,0 +1,6 @@
+'Write a Python program that will accept the base and height of a triangle and compute the area.'
+
+base = int(input('Enter base of the triangle '))
+height =  int(input('Enter height of the triangle '))
+area =  1/2*base*height
+print('Area of triangle of base',base,'and height',height,'is',area)
