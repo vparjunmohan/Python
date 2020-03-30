9059d02366e1ca4e150933bf5f21a10a0e0d7428
vparjunmohan <vparjunmohan@gmail.com>
2020-03-30 10:32:02 +0530

    p24 added

---
+++ Basics-Part-I/program24.py
@@ -0,0 +1,10 @@
+'Write a Python program to test whether a passed letter is a vowel or not.'
+
+def vowel():
+    if n in ['a','e','i','o','u']:
+        print(n,'is a vowel')
+    else:
+        print(n,'is not a vowel')
+        
+n = input('Enter a letter ')
+vowel()
