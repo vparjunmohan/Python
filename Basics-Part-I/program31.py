070fd82cad124e868691c8ddb48aabae52df03f7
vparjunmohan <vparjunmohan@gmail.com>
2020-04-01 17:15:59 +0530

    p31 added

---
+++ Basics-Part-I/program31.py
@@ -0,0 +1,7 @@
+'Write a Python program to compute the greatest common divisor (GCD) of two positive integers.'
+
+import math
+
+x = int(input('Enter first number '))
+y = int(input('Enter second number '))
+print('GCD of',x,'and',y,'is',math.gcd(x, y))
