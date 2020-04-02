ac1cb1949eb291c3c053d9e0e4277846f9301c4f
vparjunmohan <vparjunmohan@gmail.com>
2020-04-02 16:21:47 +0530

    p53 added

---
+++ Basics-Part-I/program53.py
@@ -0,0 +1,14 @@
+'Write a python program to access environment variables.'
+
+import os 
+
+# Access all environment variables 
+print('*----------------------------------*')
+print(os.environ)
+print('*----------------------------------*')
+
+# Access a particular environment variable 
+print(os.environ['HOME'])
+print('*----------------------------------*')
+print(os.environ['PATH'])
+print('*----------------------------------*')
