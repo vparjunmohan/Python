64fcaef2888418ef96c2183d71f7c9aa7ff022b0
vparjunmohan <vparjunmohan@gmail.com>
2020-04-02 15:09:58 +0530

    p38 added

---
+++ Basics-Part-I/program38.py
@@ -0,0 +1,9 @@
+'Write a Python program to solve (x + y) * (x + y).'
+
+def solve(x, y):
+    return (x + y) * (x + y)
+
+x = int(input('Enter first number '))
+y = int(input('Enter second number '))
+
+print(solve(x, y))
