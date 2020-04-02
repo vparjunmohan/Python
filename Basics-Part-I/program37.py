f362ae0182ea4d607081ee07f3e11760b92024af
vparjunmohan <vparjunmohan@gmail.com>
2020-04-02 15:06:58 +0530

    p37 added

---
+++ Basics-Part-I/program37.py
@@ -0,0 +1,9 @@
+'Write a Python program to display your details like name, age, address in three different lines.'
+
+name = input('Enter your name ')
+age = int(input('Enter your age '))
+address = input('Enter your address ')
+
+print('Name :',name)
+print('Age :', age)
+print('Address :',address)
