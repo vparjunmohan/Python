7dbc55b8ce1748fa0bab6632a70b483f59b1f8e5
vparjunmohan <vparjunmohan@gmail.com>
2020-04-10 12:02:01 +0530

    p63 added

---
+++ Basics-Part-I/program63.py
@@ -0,0 +1,6 @@
+'Write a Python program to get an absolute file path.'
+
+def absolute_file_path(path_fname):
+        import os
+        return os.path.abspath('path_fname')        
+print("Absolute file path: ",absolute_file_path("test.txt"))
