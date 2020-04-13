d5a4fb1bb88d9c88d0e78a7018cc6e5a3b1d49ae
vparjunmohan <vparjunmohan@gmail.com>
2020-04-13 17:40:30 +0530

    p92 added

---
+++ Basics-Part-I/program92.py
@@ -0,0 +1,10 @@
+'Write a Python program to define a string containing special characters in various forms.'
+
+print()
+print("\#{'}${\"}@/")
+print("\#{'}${"'"'"}@/")
+print(r"""\#{'}${"}@/""")
+print('\#{\'}${"}@/')
+print('\#{'"'"'}${"}@/')
+print(r'''\#{'}${"}@/''')
+print()
