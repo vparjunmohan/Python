01add6f35f9ea533b82dd0756bd3d6cae5b787b5
vparjunmohan <vparjunmohan@gmail.com>
2020-04-13 17:38:55 +0530

    p91 added

---
+++ Basics-Part-I/program91.py
@@ -0,0 +1,10 @@
+'Write a Python program to swap two variables.'
+
+a = input('Enter value of A ') 
+b = input('Enter value of B ') 
+temp = a 
+a = b 
+b = temp
+
+print('Swapped values are A = {}, B = {}'.format(a,b))
+
