55c9fb9fc63a05f5c83ee80a5ad7911ac1c72f01
vparjunmohan <vparjunmohan@gmail.com>
2020-04-15 21:24:17 +0530

    p100 added

---
+++ Basics-Part-I/program100.py
@@ -0,0 +1,8 @@
+'Write a Python program to get the name of the host on which the routine is running.'
+
+import socket
+
+host_name = socket.gethostname()
+print()
+print('Host name:', host_name)
+print()
