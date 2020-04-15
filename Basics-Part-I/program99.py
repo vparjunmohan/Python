dfcd250a19298a42b4b616782179920a272f6ffd
vparjunmohan <vparjunmohan@gmail.com>
2020-04-15 21:23:01 +0530

    p99 added

---
+++ Basics-Part-I/program99.py
@@ -0,0 +1,10 @@
+'Write a Python program to clear the screen or terminal.'
+
+import os
+import time
+# for windows
+# os.system('cls')
+os.system("cls")
+time.sleep(2)
+# Ubuntu version 10.10
+os.system('clear')
