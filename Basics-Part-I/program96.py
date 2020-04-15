d7f51803d10f277668e79c42046caaa704f24a5f
vparjunmohan <vparjunmohan@gmail.com>
2020-04-15 21:16:50 +0530

    p96 added

---
+++ Basics-Part-I/program96.py
@@ -0,0 +1,12 @@
+'Write a Python program to print the current call stack.'
+
+import traceback
+
+def f():
+    g()
+
+def g():
+    for line in traceback.format_stack():
+        print(line.strip())
+
+f()
