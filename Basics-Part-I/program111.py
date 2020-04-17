59176a18916ca13faa8ef2a89357b269afd5578e
vparjunmohan <vparjunmohan@gmail.com>
2020-04-17 16:11:21 +0530

    p111 added

---
+++ Basics-Part-I/program111.py
@@ -0,0 +1,6 @@
+'Write a Python program to remove the first item from a specified list.'
+
+fruitList = ['apple','orange','banana','pineapple']
+print('\nOriginal List',fruitList)
+newList =  fruitList.pop(0)
+print('\nAfter removing the first item',fruitList)
