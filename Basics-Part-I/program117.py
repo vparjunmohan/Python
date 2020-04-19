47d60bf022f7068f4e23fa94ef81ba99cb2e178e
vparjunmohan <vparjunmohan@gmail.com>
2020-04-19 21:44:03 +0530

    p117 added

---
+++ Basics-Part-I/program117.py
@@ -0,0 +1,10 @@
+'Write a Python program to create a bytearray from a list.'
+
+
+nums = [10, 20, 56, 35, 17, 99]
+# Create bytearray from list of integers.
+values = bytearray(nums)
+
+for x in values:
+    print(x)
+
