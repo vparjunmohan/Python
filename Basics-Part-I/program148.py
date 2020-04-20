e6fa91654db01f5b45649df2f03eddc971f1bfed
vparjunmohan <vparjunmohan@gmail.com>
2020-04-20 08:14:40 +0530

    p148 added

---
+++ Basics-Part-I/program148.py
@@ -0,0 +1,12 @@
+'Write a Python function that takes a positive integer and returns the sum of the cube of all the positive integers smaller than the specified number.'
+
+def sum_of_cubes(n):
+  n -= 1
+  total = 0
+  while n > 0:
+    total += n * n * n
+    n -= 1
+  return total
+
+n = int(input('Enter a number '))
+print('Sum of cubes: ',sum_of_cubes(n))
