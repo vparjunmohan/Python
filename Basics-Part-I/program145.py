7b8e6055041459f58db6f5317c7d19a0cf44aa28
vparjunmohan <vparjunmohan@gmail.com>
2020-04-20 08:04:43 +0530

    p145 added

---
+++ Basics-Part-I/program145.py
@@ -0,0 +1,9 @@
+'Write a Python program to find the location of Python module sources.'
+
+import sys
+import os
+
+print('\nList of directories in sys module:')
+print(sys.path)
+print('\nList of directories in os module:')
+print(os.path)
