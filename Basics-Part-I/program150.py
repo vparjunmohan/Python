22abf2c227488d74fc11cf9e3fa0faf16179f8d5
vparjunmohan <vparjunmohan@gmail.com>
2020-04-20 08:23:36 +0530

    p150 added

---
+++ Basics-Part-I/program150.py
@@ -0,0 +1,11 @@
+'Python Program for Sum of squares of first n natural numbers.'
+
+def squaresum() : 
+    sm = 0
+    for i in range(1, n+1): 
+        sm = sm + (i * i)
+    return sm 
+
+n = int(input('Enter a number '))
+print('Sum of squares is',squaresum()) 
+
