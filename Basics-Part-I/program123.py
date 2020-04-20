be7c45df8b92ee8e84909a6903dffb4af93829c1
vparjunmohan <vparjunmohan@gmail.com>
2020-04-20 07:01:30 +0530

    p123 added

---
+++ Basics-Part-I/program123.py
@@ -0,0 +1,8 @@
+'Write a Python program to check whether multiple variables have the same value.'
+
+x = int(input('Enter first value '))
+y = int(input('Enter second value '))
+z = int(input('Enter third value '))
+if x == y == z:
+    print("All variables have same value!")  
+	
