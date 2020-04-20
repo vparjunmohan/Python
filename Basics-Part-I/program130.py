3ffd4e160d6559851d0a48c84417d9a36ca9d794
vparjunmohan <vparjunmohan@gmail.com>
2020-04-20 07:29:03 +0530

    p130 added

---
+++ Basics-Part-I/program130.py
@@ -0,0 +1,9 @@
+'Write a Python program to split a variable length string into variables.'
+
+var_list = ['a', 'b', 'c']
+x, y, z = var_list 
+print(x, y, z)
+var_list = [100, 20.25]
+x, y = var_list 
+print(x, y)
+
