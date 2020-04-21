25d270b0a03ba0279f919b740d3b78079963ff10
vparjunmohan <vparjunmohan@gmail.com>
2020-04-21 18:24:11 +0530

    p34 added

--- Basics-Part-II/program34.py
+++ Basics-Part-II/program34.py
@@ -9,7 +9,7 @@ a,b,c,d,e,f separated by a single space.
 print('Input the value of a, b, c, d, e, f:')
 a, b, c, d, e, f = map(float, input().split())
 n = a*e - b*d
-print("Values of x and y:")
+print('Values of x and y:')
 if n != 0:
     x = (c*e - b*f) / n
     y = (a*f - c*d) / n
