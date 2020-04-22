7b6acf8509b8618daeab5b5c5b24d31ae091cd17
vparjunmohan <vparjunmohan@gmail.com>
2020-04-22 18:28:31 +0530

    p71 added

---
+++ Basics-Part-II/program71.py
@@ -0,0 +1,15 @@
+'''Write a Python program to check whether a given integer is a palindrome or not. 
+Note: An integer is a palindrome when it reads the same backward as forward. Negative numbers are not palindromic.
+Sample Output:
+False
+True
+False'''
+
+
+def is_Palindrome(n):
+    return str(n) == str(n)[::-1]
+
+
+print(is_Palindrome(100))
+print(is_Palindrome(252))
+print(is_Palindrome(-838))
