3e6636a78b4401a07be7a4b2c86060fe6fc1723d
vparjunmohan <vparjunmohan@gmail.com>
2020-05-10 11:17:46 +0530

    p3 added

---
+++ String/program3.py
@@ -0,0 +1,8 @@
+'Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string. If the string length is less than 2, return instead of the empty string.'
+
+string = input('Enter string ')
+newString = string[0:2] + string[-2:]
+if len(string) < 2:
+    print('')
+else:
+    print(newString)
